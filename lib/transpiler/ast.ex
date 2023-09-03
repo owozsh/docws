84c2e78370c4927466302fa32c3a8142dbfcd2de
Vítor Barroso <contact@owozsh.dev>
2023-09-02 22:01:34 -0300

    add scan_token

--- lib/transpiler/ast.ex
+++ lib/transpiler/ast.ex
@@ -3,6 +3,10 @@ defmodule Transpiler.AST do
     {:root, nil, []}
   end
 
+  def create_node(token, meta, children) do
+    {token, meta, children}
+  end
+
   def add_child({type, data, children}, child) do
     {type, data, [children | child]}
   end
--- lib/transpiler/lexer.ex
+++ lib/transpiler/lexer.ex
@@ -1,18 +1,37 @@
 defmodule Transpiler.Lexer do
+  alias Transpiler.AST
   alias Transpiler.Token
 
   @type ast :: {atom, map, list}
 
-  @spec scan(ast, list, integer) :: {ast, list, integer}
-  def scan(ast, chars, pos)
+  @spec scan(ast, list) :: {ast, list}
+  def scan(ast, chars)
 
-  def scan(ast, chars, pos) when length(chars) == pos do
+  def scan(ast, chars) when length(chars) == 0 do
     ast
   end
 
-  def scan(ast, chars, pos) do
-    for char <- chars do
-      token = Token.from_char(char)
+  def scan(ast, chars) do
+    token = Token.from_char(chars[0])
+
+    case token do
+      :title -> scan_token(token, ast, chars)
     end
+  end
+
+  def scan_token(:title, ast, chars) do
+    [title_text, _] =
+      chars
+      |> Enum.join()
+      |> String.split("\n")
+
+    child =
+      AST.create_node(:title, nil, [])
+      |> AST.add_child(AST.create_node(:text, title_text, []))
+
+    [chars_wo_token, _] = chars
+
+    AST.add_child(ast, child)
+    |> scan(chars_wo_token)
   end
 end
